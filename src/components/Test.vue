8efe72be437f5bf57efb333f666f7d6503ee6533
spectakural <this.is.kural987@gmail.com>
2021-08-26 22:32:27 +0530

    init

---
+++ src/components/Test.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="testpage">
+        <button @click="sendTest">click</button>
+    </div>
+</template>
+
+<script>
+    import io from "socket.io-client";
+
+    export default {
+        name: "Test",
+        data() {
+            return {
+                socket: io("http://localhost:3001")
+            }
+        },
+        methods: {
+            sendTest() {
+                this.socket.emit("sendTest", {msg: "test"})
+            }
+
+        },
+        mounted() {
+            this.socket.on("sendVideo", (data) => {
+                console.log(data)
+
+            })
+            this.socket.on("recieved", (data)=> {
+                console.log(data)
+            })
+
+        },
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <Test />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Test from "@/components/Test.vue"
+
+export default {
+  name: 'Home',
+  components: {
+    Test,
+    
+  }
+}
+</script>
